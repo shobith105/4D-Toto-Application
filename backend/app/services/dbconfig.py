9d1e44a74260d93e0b3633682fa9bd8e238dab4b
Shobith <shobith105@gmail.com>
2026-01-21 01:33:04 +0800

    Minor changes to help with testing

--- backend/app/services/dbconfig.py
+++ backend/app/services/dbconfig.py
@@ -7,8 +7,8 @@ from fastapi import Header, HTTPException,Depends
 
 
 load_dotenv()
-url=os.getenv("VITE_SUPABASE_URL")
-key=os.getenv("VITE_SUPABASE_PUBLISHABLE_DEFAULT_KEY")
+url = os.getenv("SUPABASE_URL") or os.getenv("VITE_SUPABASE_URL")
+key = os.getenv("SUPABASE_SERVICE_ROLE_KEY") or os.getenv("VITE_SUPABASE_PUBLISHABLE_DEFAULT_KEY")
 
 supabase: Client=create_client(url,key)
 
